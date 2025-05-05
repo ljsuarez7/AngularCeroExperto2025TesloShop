c964a7c262c467b4f5ab18287c7f0ec9ab8499da
JaviSrz <ljsuarez7@gmail.com>
2025-05-05 22:08:57 +0200

    paginas base del store front de la app, rutas y componentes basicos

---
+++ src/app/store-front/components/store-front-navbar/store-front-navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'store-front-navbar',
+  imports: [],
+  templateUrl: './store-front-navbar.component.html',
+})
+export class StoreFrontNavbarComponent { }
--- src/app/store-front/store-front.routes.ts
+++ src/app/store-front/store-front.routes.ts
@@ -1,11 +1,33 @@
 import { Routes } from "@angular/router";
 import { StoreFrontLayoutComponent } from "./layouts/store-front-layout/store-front-layout.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { GenderPageComponent } from "./pages/gender-page/gender-page.component";
+import { ProductPageComponent } from "./pages/product-page/product-page.component";
+import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
 
 export const storeFrontRoutes: Routes = [
   {
     path: '',
     component: StoreFrontLayoutComponent,
-    children: []
+    //Se podria cargar las rutas del children con lazy load para que cargue solo la pagina hija si vemos que crece mucho
+    children: [
+      {
+        path: '',
+        component: HomePageComponent
+      },
+      {
+        path: 'gender/:gender',
+        component: GenderPageComponent
+      },
+      {
+        path: 'product/:idSlug',
+        component: ProductPageComponent
+      },
+      {
+        path: '**',
+        component: NotFoundPageComponent
+      }
+    ]
   },
   {
     path: '**',
