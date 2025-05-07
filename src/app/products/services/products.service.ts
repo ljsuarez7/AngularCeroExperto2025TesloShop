5b0d060958bb89f269b57878e3785fe29c5a4806
JaviSrz <ljsuarez7@gmail.com>
2025-05-07 22:04:08 +0200

    paginas de producto

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { ProductsResponse } from '@products/interfaces/product.interface';
+import { Product, ProductsResponse } from '@products/interfaces/product.interface';
 import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -30,10 +30,18 @@ export class ProductsService {
         offset,
         gender
       }
-    })
-    .pipe(
-      tap(resp => console.log(resp))
-    );
+    });
+    // .pipe(
+    //   tap(resp => console.log(resp))
+    // );
+
+  }
+
+  getProductByIdSlug(idSlug: string): Observable<Product> {
+
+    return this.http
+    .get<Product>(`${baseUrl}/products/${idSlug}`);
+
 
   }
 
--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '@products/interfaces/product.interface';
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
   selector: 'app-product-page',
   imports: [],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+
+  activatedRoute = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  productIdSlug: string = this.activatedRoute.snapshot.params['idSlug'];
+
+  productResource = rxResource({
+    request: () => ({idSlug: this.productIdSlug}),
+    loader: ({request}) => this.productsService.getProductByIdSlug(request.idSlug)
+  });
+
+}
