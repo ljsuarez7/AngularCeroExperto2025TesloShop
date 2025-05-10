ee17435855d09e152dbed47bfdd0ca20dc0029f5
JaviSrz <ljsuarez7@gmail.com>
2025-05-10 09:58:19 +0200

    paginacion en la pagina por generos

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -3,17 +3,20 @@ import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './gender-page.component.html',
 })
 export class GenderPageComponent {
 
   route = inject(ActivatedRoute);
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
   gender = toSignal(
     this.route.params.pipe(
@@ -23,10 +26,11 @@ export class GenderPageComponent {
 
   //Se podria implementar una caché para no estar pidiendo esto todo el rato, mirar en curso angular pro se hacer con una libreria
   productsResource = rxResource({
-    request: () => ({gender: this.gender()}),
+    request: () => ({gender: this.gender(), page: this.paginationService.currentPage() - 1}),
     loader: ({request}) => {
       return this.productsService.getProducts({
-        gender: request.gender
+        gender: request.gender,
+        offset: request.page * 9
       });
     }
   });
