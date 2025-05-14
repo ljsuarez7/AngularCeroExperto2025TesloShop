0152eb4987ee99e47ba47fa2c3f5dcaa98ed636d
JaviSrz <ljsuarez7@gmail.com>
2025-05-14 19:04:34 +0200

    Creamos los botones para el login y salir de sesión

--- src/app/store-front/components/store-front-navbar/store-front-navbar.component.ts
+++ src/app/store-front/components/store-front-navbar/store-front-navbar.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'store-front-navbar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './store-front-navbar.component.html',
 })
-export class StoreFrontNavbarComponent { }
+export class StoreFrontNavbarComponent {
+
+  authService = inject(AuthService);
+
+}
