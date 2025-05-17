4e6c4283101a6e05f1f2b3b5735a156edfd3ac1c
JaviSrz <ljsuarez7@gmail.com>
2025-05-17 12:52:13 +0200

    Algunas correcciones visuales

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -8,6 +8,8 @@ import { RouterLink } from '@angular/router';
 })
 export class PaginationComponent {
 
+  //TODO: Revisar la paginación, porque me da la sensación de que muestra elementos repetidos en las paginas
+
   pages = input<number>(0);
   currentPage = input<number>(1);
 
