da5b236e036e01c2ed44f3d00a128c27a7ddc13c
JaviSrz <ljsuarez7@gmail.com>
2025-05-18 13:05:50 +0200

    Mostrar datos del producto en el formulario y hacer funcionar los botones de genero y tallas y poder guardar los tags

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductCarouselComponent } from '@products/components/product-carousel/product-carousel.component';
 import { Product } from '@products/interfaces/product.interface';
@@ -9,7 +9,7 @@ import { FormUtils } from '@utils/form-utils';
   imports: [ProductCarouselComponent, ReactiveFormsModule],
   templateUrl: './product-details.component.html',
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
 
   product = input.required<Product>();
 
@@ -23,11 +23,40 @@ export class ProductDetailsComponent {
     stock: [0, [Validators.required, Validators.min(0)]],
     sizes: [['']],
     images: [[]],
-    tags: [['']],
+    tags: [''],
     gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]]
   });
 
   sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  ngOnInit(): void {
+    this.setFormValue(this.product());
+  }
+
+  setFormValue(formLike: Partial<Product>) {
+
+    this.productForm.reset(this.product() as any);
+
+    this.productForm.patchValue({tags: formLike.tags?.join(',')});
+
+    // this.productForm.patchValue(formLike as any);
+
+
+  }
+
+  onSizeClicked(size: string) {
+
+    const currentSizes = this.productForm.value.sizes ?? [];
+
+    if(currentSizes.includes(size)) {
+      currentSizes.splice(currentSizes.indexOf(size), 1);
+    }else {
+      currentSizes.push(size);
+    }
+
+    this.productForm.patchValue({sizes: currentSizes});
+
+  }
 
   onSubmit(){
     console.log(this.productForm.value);
