af08138fc82bbccdee6f0d088949443996b862a2
JaviSrz <ljsuarez7@gmail.com>
2025-05-21 19:33:02 +0200

    Añadimos el withHashLocation para facilitar el despliegue

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -9,7 +9,7 @@ import { authInterceptor } from '@auth/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()), //el withHashLocation añade un # a la ruta #/home, sirve para desplegarlo y poder recargar la pagina sin perder la pagina en la que estamos
     provideHttpClient(
       withFetch(),
       withInterceptors([authInterceptor])
